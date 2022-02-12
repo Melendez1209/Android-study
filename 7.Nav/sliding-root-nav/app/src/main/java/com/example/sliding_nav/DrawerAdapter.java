62e9c91b5a44a5079aacdc283e523fd29273b313
Melendez <suge1209@163.com>
2022-02-12 17:17:16 +0800

    适配器必要函数添加

--- 7.Nav/sliding-root-nav/app/src/main/java/com/example/sliding_nav/DrawerAdapter.java
+++ 7.Nav/sliding-root-nav/app/src/main/java/com/example/sliding_nav/DrawerAdapter.java
@@ -1,11 +1,35 @@
 package com.example.sliding_nav;
 
 import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+import java.util.Map;
+
 public class DrawerAdapter extends RecyclerView.Adapter<DrawerAdapter.ViewHolder> {
+    //全局变量
+    private List<Drawer_item> items;
+    private Map<Class<? extends Drawer_item>, Integer> ViewTepys;
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
     static abstract class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         //全局变量
         private DrawerAdapter drawerAdapter;
