223ce0bb1d0a55a07f6c5cf605cd9e249c299530
Melendez <suge1209@163.com>
2022-02-13 13:19:39 +0800

    空格项目-声明与构造函数

--- 7.Nav/sliding-root-nav/app/src/main/java/com/example/sliding_nav/SpaceItem.java
+++ 7.Nav/sliding-root-nav/app/src/main/java/com/example/sliding_nav/SpaceItem.java
@@ -1,4 +1,34 @@
 package com.example.sliding_nav;
 
-public class SpaceItem {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+
+public class SpaceItem extends Drawer_item<SpaceItem.ViewHolder> {
+    //全局变量
+    //声明与构造函数
+    private int spaceDp;
+
+    public SpaceItem(int spaceDp) {
+        this.spaceDp = spaceDp;
+    }
+
+    @Override
+    public ViewHolder createViewHolder(ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public void bindViewHolder(ViewHolder Holder) {
+
+    }
+
+    //内部视图持有人类
+    public class ViewHolder extends DrawerAdapter.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
