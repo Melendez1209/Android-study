3b1862153ea7d27800eb4c07dda99e8c850dc2c2
Melendez <suge1209@163.com>
2022-02-13 13:56:48 +0800

    main-滑动根导航变量

--- 7.Nav/sliding-root-nav/app/src/main/java/com/example/sliding_nav/MainActivity.java
+++ 7.Nav/sliding-root-nav/app/src/main/java/com/example/sliding_nav/MainActivity.java
@@ -1,11 +1,26 @@
 package com.example.sliding_nav;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class MainActivity extends AppCompatActivity {
+    //全局变量
+    //页面
+    private final static int POS_CLOSE = 0;
+    private final static int POS_CLOSE = 0;
+    private final static int POS_CLOSE = 0;
+    private final static int POS_CLOSE = 0;
+    private final static int POS_CLOSE = 0;
+
+    //2个用于传递项目名称及图标的字符串列表
+    private String[] screenTitles;
+    private Drawable[] screenIcon;
+
+    //滑动根导航变量
+    //16:39
+    private 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
