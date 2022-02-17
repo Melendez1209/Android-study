30c27f44fc8dfd12ad50afe5d67a574a65cb4b91
Melendez <suge1209@163.com>
2022-02-17 19:28:29 +0800

    MainActivity.java

--- 6.ViewPage/app/src/main/java/com/example/viewpage/MainActivity.java
+++ 6.ViewPage/app/src/main/java/com/example/viewpage/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //控件调取
         ViewPager2 viewpage2 = findViewById(R.id.viewpager2);
+        //传入适配器
         ViewPagerAdapter ViewPagerAdapter = new ViewPagerAdapter();
         viewpage2.setAdapter(ViewPagerAdapter);
     }
