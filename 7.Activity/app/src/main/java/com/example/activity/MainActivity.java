6d31d60760f7f3912aad9b7b7f758aa2fdd132ac
Melendez <suge1209@163.com>
2022-02-20 14:27:33 +0800

    Activity跳转

--- 7.Activity/app/src/main/java/com/example/activity/MainActivity.java
+++ 7.Activity/app/src/main/java/com/example/activity/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    //Activity跳转
+    public void go_to_activity2(View view) {
+        startActivity(new Intent(this,MainActivity2.class));
     }
 }
