16ec232e148dfb103117603297500c9d12620c0d
Melendez <suge1209@163.com>
2022-02-20 19:50:23 +0800

    new Receiver.java

---
+++ 10.Receiver/app/src/main/java/com/melendez/Receiver/Receiver.java
@@ -0,0 +1,13 @@
+package com.melendez.Receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+//接收者
+public class Receiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
