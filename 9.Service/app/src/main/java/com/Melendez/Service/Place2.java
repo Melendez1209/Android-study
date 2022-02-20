d8e5a279e77e9a723ea3e0eb296518f6e3fecc46
Melendez <suge1209@163.com>
2022-02-20 16:31:00 +0800

    跳转至领域2完结

--- 9.Service/app/src/main/java/com/Melendez/Service/Place2.java
+++ 9.Service/app/src/main/java/com/Melendez/Service/Place2.java
@@ -1,8 +1,10 @@
 package com.Melendez.Service;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Place2 extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class Place2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place2);
+    }
+
+    public void StartServce(View view) {
+        startService(new Intent(this, Service.class));//启动服务
+    }
+
+    public void StopServce(View view) {
+        stopService(new Intent(this, Service.class));//停止服务
+    }
+
+    //跳转至领域2
+    public void go_to_place1(View view) {
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
