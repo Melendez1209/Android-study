6770b6e555f6288d3a15e3ccdaf862a40bc29f77
Melendez <suge1209@163.com>
2022-02-26 17:09:01 +0800

    静态广播完结，等待调试

--- 10.Receiver/app/src/main/java/com/melendez/Receiver/MainActivity.java
+++ 10.Receiver/app/src/main/java/com/melendez/Receiver/MainActivity.java
@@ -1,5 +1,6 @@
 package com.melendez.Receiver;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
 
     //静态发送广播给接收者
     public void send_receiver(View view) {
-
+        Intent intent = new Intent();
+        intent.setAction(ActionUtils.ACTION_FLAG);//与注册保持一致的凭证
+        sendBroadcast(intent);
     }
 }
