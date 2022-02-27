f40ce1e4a5c645cbcb1f6845ff5450a710927c4c
Melendez <suge1209@163.com>
2022-02-27 17:41:57 +0800

    new SettingsActivity

--- Practical operation/2.BMI/app/src/main/java/com/melendez/bmi/MainActivity.java
+++ Practical operation/2.BMI/app/src/main/java/com/melendez/bmi/MainActivity.java
@@ -2,6 +2,7 @@ package com.melendez.bmi;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //控件
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        //进入设置Activity
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
     //读取返回值
