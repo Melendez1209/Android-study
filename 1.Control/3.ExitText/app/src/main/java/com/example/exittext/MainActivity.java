26d4f2ad688d75be5fe8085afeeaa7c592e10ae9
Melendez <suge1209@163.com>
2022-03-12 08:28:27 +0800

    代码优化

--- 1.Control/3.ExitText/app/src/main/java/com/example/exittext/MainActivity.java
+++ 1.Control/3.ExitText/app/src/main/java/com/example/exittext/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.exittext;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,14 +15,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button login = findViewById(R.id.login);
         user_name = findViewById(R.id.user_name);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String user_name_text = user_name.getText().toString();
-                Log.e("Melendez", "用户名" + user_name_text);
-            }
-        });
+    }
+
+    public void click(View view) {
+        String user_name_text = user_name.getText().toString();
+        Log.e("Melendez", "用户名" + user_name_text);
     }
 }
